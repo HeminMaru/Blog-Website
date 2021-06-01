94e5d021e07f713950386d0d59e60bd2c33138cd
Hemin Maru <heminmaru@Hemins-MacBook-Pro.local>
2021-06-02 02:52:53 +0530

    connected to database

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const _ = require('lodash');
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -16,25 +17,40 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-const composes = [];
+mongoose.connect('mongodb://localhost:27017/BlogWebsite', { useNewUrlParser: true, useUnifiedTopology: true });
+const Blog = mongoose.model('Blog', {
+    title: {
+        type: String,
+        require: [true, "Title not found"]
+    },
+    post: {
+        type: String,
+        require: [true, "Post not found"]
+    }
+});
 
 app.get("/", function(req, res) {
     res.render("home", { startingContent: homeStartingContent });
 });
 
 app.get("/blogs", function(req, res) {
-    res.render("blogs", { content: composes });
+    Blog.find({}, function(err, blogs) {
+        if (err) {
+            console.log(err);
+        }
+        console.log(blogs);
+        res.render("blogs", { content: blogs });
+    });
 });
 
 app.get("/blogs/:blogTitle", function(req, res) {
-    title = _.lowerCase(req.params.blogTitle);
-    var selectedBlog = {};
-    composes.forEach(function(blog) {
-        if (_.lowerCase(blog.title) === title) {
-            selectedBlog = blog;
+    blogT = _.lowerCase(req.params.blogTitle);
+    Blog.find({ title: blogT }, function(err, selectedBlog) {
+        if (err) {
+            console.log(err);
         }
-    })
-    res.render("post", { blog: selectedBlog });
+        res.render("post", { blog: selectedBlog[0] });
+    });
 });
 
 app.get("/about", function(req, res) {
@@ -50,11 +66,15 @@ app.get("/compose", function(req, res) {
 });
 
 app.post("/compose", function(req, res) {
-    const compose = {
+    const newBlog = new Blog({
         title: req.body.newComposeTitle,
         post: req.body.newComposePost
-    };
-    composes.push(compose);
+    });
+    Blog.insertMany(newBlog, function(err) {
+        if (err) {
+            console.log(err);
+        }
+    });
     res.redirect("/");
 });
 
